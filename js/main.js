eda10cbc7ca2cb8f23c3c40e6a9b41a363e60d9c
Владислав Медяннкин <medyankin03@mail.ru>
2022-11-14 20:08:43 +0700

    Исправил таблицу успеваемости

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function EditColorButton(btn1, btn2) 
+{
+    btn1.style.color = "black";
+    btn2.style.color = "rgb(202, 12, 12)";
+}
+
+function DrawTable(semestr) 
+{
+    var k = document.getElementById(semestr);
+    var btn1 = document.getElementById("btn1");
+    var btn2 = document.getElementById("btn2");
+
+    if (semestr == "1semester") 
+    {
+        EditColorButton(btn2, btn1)
+        var k2 = document.getElementById("2semester");
+    }
+
+    else 
+    {
+        EditColorButton(btn1, btn2);
+        var k2 = document.getElementById("1semester");
+    }
+
+    k2.style.display = "none";
+    k.style.display == "none" ? k.style.display = "table" : k.style.display = "table";
+}
